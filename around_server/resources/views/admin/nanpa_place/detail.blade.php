6804bbdebe6137acdf8d87abdbd9555bdd73aac0
nakamura <fuji.nakamura.a@gimail.com>
2020-12-17 20:09:54 +0900

    管理画面

--- around_server/resources/views/admin/nanpa_place/detail.blade.php
+++ around_server/resources/views/admin/nanpa_place/detail.blade.php
@@ -31,62 +31,61 @@
 
                                     <tbody>
                                     <tr>
-<td>name</td>
-<td>{{ $detail['place_name'] }}</td>
-</tr>
-<tr>
-<td>name</td>
-<td>{{ $detail['genre'] }}</td>
-</tr>
-</tr>
-<tr>
-<td>name</td>
-<td>{{ $detail['score'] }}</td>
-</tr>
-<tr>
-<td>name</td>
-<td>{{ $detail['longitude_latitude'] }}</td>
-</tr>
-<tr>
-<td>name</td>
-<td>{{ $detail['longitude'] }}</td>
-</tr>
-<tr>
-<td>name</td>
-<td>{{ $detail['latitude'] }}</td>
-</tr>
-<tr>
-<td>name</td>
-<td>{{ $detail['open_flag'] }}</td>
-</tr>
-<tr>
-<td>name</td>
-<td>{{ $detail['ratio'] }}</td>
-</tr>
-<tr>
-<td>name</td>
-<td>{{ $detail['icon'] }}</td>
-</tr>
-<tr>
-<td>name</td>
-<td>{{ $detail['start_time'] }}</td>
-</tr>
-<tr>
-<td>name</td>
-<td>{{ $detail['end_time'] }}</td>
-</tr>
-<tr>
-<td>name</td>
-<td>{{ $detail['start_age_group'] }}</td>
-</tr>
-<tr>
-<td>name</td>
-<td>{{ $detail['end_age_group'] }}</td>
-</tr>
-<tr>
-<td>name</td>
-<td>{{ $detail['memo'] }}</td>
-</tr>
+                                        <td>name</td>
+                                        <td>{{ $detail['place_name'] }}</td>
+                                    </tr>
+                                    <tr>
+                                        <td>name</td>
+                                        <td>{{ $detail['genre'] }}</td>
+                                    </tr>
+                                    <tr>
+                                        <td>name</td>
+                                        <td>{{ $detail['score'] }}</td>
+                                    </tr>
+                                    <tr>
+                                        <td>name</td>
+                                        <td>{{ $detail['longitude_latitude'] }}</td>
+                                    </tr>
+                                    <tr>
+                                        <td>name</td>
+                                        <td>{{ $detail['longitude'] }}</td>
+                                    </tr>
+                                    <tr>
+                                        <td>name</td>
+                                        <td>{{ $detail['latitude'] }}</td>
+                                    </tr>
+                                    <tr>
+                                        <td>name</td>
+                                        <td>{{ $detail['open_flag'] }}</td>
+                                    </tr>
+                                    <tr>
+                                        <td>name</td>
+                                        <td>{{ $detail['ratio'] }}</td>
+                                    </tr>
+                                    <tr>
+                                        <td>name</td>
+                                        <td>{{ $detail['icon'] }}</td>
+                                    </tr>
+                                    <tr>
+                                        <td>name</td>
+                                        <td>{{ $detail['start_time'] }}</td>
+                                    </tr>
+                                    <tr>
+                                        <td>name</td>
+                                        <td>{{ $detail['end_time'] }}</td>
+                                    </tr>
+                                    <tr>
+                                        <td>name</td>
+                                        <td>{{ $detail['start_age_group'] }}</td>
+                                    </tr>
+                                    <tr>
+                                        <td>name</td>
+                                        <td>{{ $detail['end_age_group'] }}</td>
+                                    </tr>
+                                    <tr>
+                                        <td>name</td>
+                                        <td>{{ $detail['memo'] }}</td>
+                                    </tr>
                                     </tbody>
                                 </table>
                             </div>
