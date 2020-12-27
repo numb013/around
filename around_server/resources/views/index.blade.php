7dab07a00c704037e5e75e7a6d42ee8f158e1780
中村篤史 <nakamuraatsushi@nakamuraatsushinoMacBookPro.local>
2020-12-27 17:58:45 +0900

    位置情報

--- around_server/resources/views/index.blade.php
+++ around_server/resources/views/index.blade.php
@@ -4,43 +4,29 @@
         @section('head')  
             @include('layouts.head')  
         @show 
-        <!-- <script src="https://cdn.jsdelivr.net/npm/vue@2.5.16/dist/vue.js"></script> -->
-          <script>
-            // //ユーザーの現在の位置情報を取得
-            // navigator.geolocation.getCurrentPosition(successCallback, errorCallback);
-            // /***** ユーザーの現在の位置情報を取得 *****/
-            // function successCallback(position) {
-            //   var gl_text = "緯度：" + position.coords.latitude + "<br>";
-            //     gl_text += "経度：" + position.coords.longitude + "<br>";
-            //     gl_text += "高度：" + position.coords.altitude + "<br>";
-            //     gl_text += "緯度・経度の誤差：" + position.coords.accuracy + "<br>";
-            //     gl_text += "高度の誤差：" + position.coords.altitudeAccuracy + "<br>";
-            //     gl_text += "方角：" + position.coords.heading + "<br>";
-            //     gl_text += "速度：" + position.coords.speed + "<br>";
-            //   document.getElementById("show_result").innerHTML = gl_text;
-            // }
-            // /***** 位置情報が取得できない場合 *****/
-            // function errorCallback(error) {
-            //   var err_msg = "";
-            //   switch(error.code)
-            //   {
-            //     case 1:
-            //       err_msg = "位置情報の利用が許可されていません";
-            //       break;
-            //     case 2:
-            //       err_msg = "デバイスの位置が判定できません";
-            //       break;
-            //     case 3:
-            //       err_msg = "タイムアウトしました";
-            //       break;
-            //   }
-            //   document.getElementById("show_result").innerHTML = err_msg;
-            //   //デバッグ用→　document.getElementById("show_result").innerHTML = error.message;
+        <script>
+            function test() {
+                
+                navigator.geolocation.getCurrentPosition(test2);
+            }
+            function test2(position) {
+                alert("ssssss");
+                var geo_text = "緯度:" + position.coords.latitude + "\n";
+                geo_text += "経度:" + position.coords.longitude + "\n";
+                geo_text += "高度:" + position.coords.altitude + "\n";
+                geo_text += "位置精度:" + position.coords.accuracy + "\n";
+                geo_text += "高度精度:" + position.coords.altitudeAccuracy  + "\n";
+                geo_text += "移動方向:" + position.coords.heading + "\n";
+                geo_text += "速度:" + position.coords.speed + "\n";
+                var date = new Date(position.timestamp);
+                geo_text += "取得時刻:" + date.toLocaleString() + "\n";
+                alert(geo_text);
             }
         </script>
     </head>
     <body>
         <div class="flex-center position-ref full-height">
+<button onclick="test()">test</button>
             <div class="content">
                 <div id="banner" class="banner_center">
                 @section('add_banner')
