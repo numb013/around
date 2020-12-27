5eecb47191521cd8a6d0998f622964f7e7f7081a
中村篤史 <nakamuraatsushi@nakamuraatsushinoMacBookPro.local>
2020-12-27 19:19:30 +0900

    位置情報

--- around_server/resources/views/index.blade.php
+++ around_server/resources/views/index.blade.php
@@ -26,24 +26,15 @@
 
         <script type="text/javascript">
     
-var param = location.search;
-console.log(param);
+            var param = location.search;
 
-
-
-            console.log("1111111111");
             var element=document.getElementById("test")
-            console.log("2222222222");
             navigator.geolocation.getCurrentPosition(successCallback, errorCallback);
-            console.log("33333333333");
             function successCallback(position) {    //成功時の処理
-                console.log("44444444444");
                 var latitude = position.coords.latitude;
                 var longitude = position.coords.longitude;
                 if (!param) {
                     if(latitude){   //変数latitudeに値が入ってた時
-                        console.log("5555555555555");
-                        alert("5555555555555");
                         getmap = confirm("位置情報の取得を開始します");//取得開始のアラート
                         if ( getmap == true ){
                             location.href = "/?latitudecd=" + latitude + "&longitudecd=" + longitude;//取得したらリダイレクト
